f9aa71df07081ae2ce25dab48350de3b84818e40
preciousNu <nprecious533@gmail.com>
2023-04-25 17:33:03 -0700

    Betty would be proud

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * main - subtract integers
+ * @a: first integer 
+ * @b: second integer
+ *
+ * Return : 0 Always
+ */
+int main(int a, int b)
+{
+	int result;
+
+	result = a - b;
+	printf("The result is %d \n", result);
+
+	return (0);
+}	
