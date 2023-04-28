f3d70d9df0361f07fc40c2940217fcd42519464f
Chinaxnaj <chinakz109@gmail.com>
2023-04-28 09:18:11 -0700

    new

--- shell.c
+++
@@ -1,8 +0,0 @@
-#include "shell.h"
-
-int main(int ac, char **av, char **env)
-{
-	if (ac == 1)
-		prompt(av, env);
-	return (0);
-}
--- shell.h
+++
@@ -1,6 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-typedef long int ssize_t;
-void prompt(char **av, char **env);
-
-#endif
